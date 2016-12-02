b68048a0147e24ff09fa39fb026ef541baf17fd0
Thomas <thomas.hansson85@gmail.com>
2016-12-02 09:27:39 +0100

    Changed all dirs for easier switch between computers.

--- src/first_server/Server.java
+++ src/first_server/Server.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Server {
 
 
-    public String loadContacts(){
+    public String loadContacts() {
         String externalContacts = "";
         Scanner scanner = null;
 
@@ -17,18 +17,13 @@ public class Server {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-
-        while(scanner.hasNext()){
-                externalContacts += scanner.nextLine().replace(',',' ') + "\n";
-    }
-    //scanner.close();
-    return externalContacts;
+        while (scanner.hasNext()) {
+            externalContacts += scanner.nextLine().replace(',', ' ') + "\n";
+        }
+          scanner.close();
+        return externalContacts;
 
     }
-
-
-
 
 
     public void serverTest() {
@@ -54,18 +49,14 @@ public class Server {
                                     BufferedReader reader = new BufferedReader(inputStreamReader);
 
                                     for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                                        System.out.println(line);
 
-                                        if(line.equals("getAll")){
+                                        if (line.equals("getAll")) {
 
-                                                writer.println(loadContacts());
+                                            writer.println(loadContacts());
 
-                                            //writer.println("Message received: " + line);
                                             writer.flush();
                                             System.out.println("contacts sent to addressbook");
                                         }
-
-
                                         if (line.equals("exit")) {
                                             break;
                                         }
@@ -75,8 +66,8 @@ public class Server {
                                     writer.close();
                                     clientSocket.close();
                                 } catch (Exception e) {
-                                    System.out.println("Testing");
-                                    //e.printStackTrace();
+
+                                    e.printStackTrace();
                                 }
                             }
 
@@ -86,8 +77,7 @@ public class Server {
             }
 
         } catch (Exception e) {
-            System.out.println("test");
-            //e.printStackTrace();
+            e.printStackTrace();
         }
     }
 
